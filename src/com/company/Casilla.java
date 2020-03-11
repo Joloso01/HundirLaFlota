5420bb3e7a503dc90846c7d28ff6817c923cc470
asixdam1b <asixdam1b>
2020-03-11 17:40:28 +0100

    hola

---
+++ src/com/company/Casilla.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Casilla {
+    String agua (String casilla){
+        casilla = " ";
+        return casilla;
+    }
+    String tiroF (String casilla){
+        casilla = "#";
+        return casilla;
+    }
+    String tiroA (String casilla){
+        casilla = "@";
+        return casilla;
+}
