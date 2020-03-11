7c89dc6991157d108344f46b8601d47a13d46aa9
asixdam1b <asixdam1b>
2020-03-11 17:41:20 +0100

    hola

--- src/com/company/Casilla.java
+++ src/com/company/Casilla.java
@@ -1,15 +1,16 @@
 package com.company;
 
 public class Casilla {
-    String agua (String casilla){
-        casilla = " ";
-        return casilla;
+        String agua (String casilla){
+            casilla = " ";
+            return casilla;
+        }
+        String tiroF (String casilla){
+            casilla = "#";
+            return casilla;
+        }
+        String tiroA (String casilla){
+            casilla = "@";
+            return casilla;
     }
-    String tiroF (String casilla){
-        casilla = "#";
-        return casilla;
-    }
-    String tiroA (String casilla){
-        casilla = "@";
-        return casilla;
 }
