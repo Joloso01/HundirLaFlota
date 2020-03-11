839badfc8234d29cb72bfa45c06a7ec5324f9535
asixdam1b <asixdam1b@asix>
2020-03-11 17:22:05 +0100

    hola3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
        String hola = "mis muertos";
+
+       int [][] tablero1 = new int[5][5];
     }
 }
